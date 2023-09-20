8bba1fe100a90b3a47e5db869174034ae9d615fe
Matthieu Flce <flacelierematthieu@gmail.com>
2023-09-20 19:26:06 +0200

    feat: add base sorting system

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,23 @@
-import './style.css'
+import "./style.css";
 
 const MIN_LIST_SIZE = 10;
+const LIST: Array<number> = [];
 
-const barContainer: HTMLDivElement = document.querySelector('.bar-container') as HTMLDivElement
-const listSizeInput = document.querySelector('.list-size') as HTMLInputElement;
-const listSizeValue = document.querySelector('.list-size-value') as HTMLInputElement
+const barContainer: HTMLDivElement = document.querySelector(".bar-container") as HTMLDivElement;
+const listSizeInput = document.querySelector(".list-size") as HTMLInputElement;
+const listSizeValue = document.querySelector(".list-size-value") as HTMLInputElement;
 
 /**
  * MAIN FUNCTION
  */
-const main = (): void => {  
+const main = (): void => {
   // Set default list size
-  listSizeInput.value = MIN_LIST_SIZE.toString()
-  updateListSizeValue()
+  listSizeInput.value = MIN_LIST_SIZE.toString();
+  updateListSizeValue();
 
   // Generate new list
-  generateNewList(parseInt(listSizeInput.value))
-}
+  generateNewList(parseInt(listSizeInput.value));
+};
 
 //#region Utils functions
 
@@ -28,28 +29,28 @@ const main = (): void => {
  * @example generateNewList(10)
  */
 const generateNewList = (length: number = 10) => {
-  const list: Array<number> = generateList(length)
-  barContainer.innerText = ''
-  addBarToContainer(list, barContainer)
-}
+  generateList(length);
+  barContainer.innerText = "";
+  addBarToContainer(LIST, barContainer);
+};
 
 /**
  * Generate list
  * @param length
  * @param max
  * @param min
- * @returns Array<number>
  * @description Generate a list of random numbers
  * @example generateList(10, 100, 5)
  */
-const generateList = (length: number, max: number = 100, min: number = 5): Array<number> => {
-  const list: Array<number> = []
+const generateList = (
+  length: number,
+  max: number = 100,
+  min: number = 5
+): void => {
   for (let i = 0; i <= length; i++) {
-    list.push(Math.floor(Math.random() * (max - min + 1) + min))
+    LIST.push(Math.floor(Math.random() * (max - min + 1) + min));
   }
-
-  return list
-}
+};
 
 /**
  * Add bar to container
@@ -59,23 +60,26 @@ const generateList = (length: number, max: number = 100, min: number = 5): Array
  * @description Add bar to container element passed in parameter
  * @example addBarToContainer([10, 20, 30], document.querySelector('.bar-container'))
  */
-const addBarToContainer = (list: Array<number>, container: HTMLElement): void => {
-  let x = 0
+const addBarToContainer = (
+  list: Array<number>,
+  container: HTMLElement
+): void => {
+  let x = 0;
   list.forEach((item: number) => {
-    const bar = document.createElement('div')
-    bar.classList.add('bar')
-    bar.id = `bar-${x}`
-    bar.style.height = `${item}%`
-    bar.style.width = `${100 / list.length}%`
-    bar.style.margin = `0 ${100 / list.length / 2}px`
-    container.appendChild(bar)
-    x++
-  })
-}
+    const bar = document.createElement("div");
+    bar.classList.add("bar");
+    bar.id = `bar-${x}`;
+    bar.style.height = `${item}%`;
+    bar.style.width = `${100 / list.length}%`;
+    bar.style.margin = `0 ${100 / list.length / 2}px`;
+    container.appendChild(bar);
+    x++;
+  });
+};
 
 // const GenerateButton = document.querySelector('.generate-btn') as HTMLButtonElement
 
-// GenerateButton.addEventListener('click', () => {  
+// GenerateButton.addEventListener('click', () => {
 //   generateNewList(parseInt(listSizeInput.value))
 // })
 
@@ -87,7 +91,59 @@ const addBarToContainer = (list: Array<number>, container: HTMLElement): void =>
 const updateListSizeValue = () => {
   let value: number = parseInt(listSizeInput.value);
   listSizeValue.innerText = value.toString();
-}
+};
+
+/**
+ * Sleep
+ * @param ms
+ */
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+/**
+ * Set bar color
+ * @param Bar id
+ */
+const setBarColor = (id: number, color: string) => {
+  const bar = document.getElementById(`bar-${id}`) as HTMLDivElement;
+  bar.style.backgroundColor = color;
+};
+
+/**
+ * Reset bar color
+ * @param Bar id
+ */
+const resetBarColor = (id: number) => {
+  const bar = document.getElementById(`bar-${id}`) as HTMLDivElement;
+  bar.style.backgroundColor = "white";
+};
+
+/**
+ * Reset all bar color
+ * @param start index
+ */
+const resetAllBarColor = (start: number = 0) => {
+  for (let i = start; i < LIST.length; i++) {
+    resetBarColor(i);
+  }
+};
+
+/**
+ * Swap bar
+ * @param id1
+ * @param id2
+ * @description Swap bar with id1 and id2
+ * @example swapBar(0, 1)
+ */
+const swapBar = (id1: number, id2: number) => {
+  const bar1 = document.getElementById(`bar-${id1}`) as HTMLDivElement;
+  const bar2 = document.getElementById(`bar-${id2}`) as HTMLDivElement;
+
+  const tmp = bar1.style.height;
+  bar1.style.height = bar2.style.height;
+  bar2.style.height = tmp;
+};
 
 //#endregion Utils functions
 
@@ -96,41 +152,54 @@ const updateListSizeValue = () => {
  * List size input event
  * @description Update list size value and generate new list
  */
-listSizeInput.addEventListener('input', function() {
+listSizeInput.addEventListener("input", function () {
   const value: number = parseInt(this.value);
 
-  generateNewList(value)
-  
-  updateListSizeValue()
-})
+  generateNewList(value);
+
+  updateListSizeValue();
+});
+
+/**
+ * Sort button event
+ * @description Sort list with selection sort algorithm
+ */
+const sortButton = document.querySelector(".sort-btn") as HTMLButtonElement;
+sortButton.addEventListener("click", () => {
+  selectionSort(LIST);
+});
 
 //#endregion Events
 
 //#region Sort functions
 
 /**
- * Bubble sort
- * @param list
- * @returns Array<number>
- * @description Sort list with bubble sort algorithm
+ * Selection sort
+ * @description Sort list with selection sort algorithm
  */
-/*const bubbleSort = (list: Array<number>): Array<number> => {
-  let swapped: boolean = false
-  do {
-    swapped = false
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] > list[i + 1]) {
-        const tmp = list[i]
-        list[i] = list[i + 1]
-        list[i + 1] = tmp
-        swapped = true
+const selectionSort = async (list: Array<number>) => {
+  for (let i = 0; i < list.length; i++) {
+    let min = i;
+    setBarColor(i, "green");
+    for (let j = i + 1; j < list.length; j++) {
+      setBarColor(j, "red");
+      if (list[j] < list[min]) {
+        min = j;
       }
+      await sleep(500);
     }
-  } while (swapped)
-
-  return list
-}*/
+    if (min !== i) {
+      let tmp = list[i];
+      list[i] = list[min];
+      list[min] = tmp;
+      await swapBar(i, min);
+    }
+    setBarColor(i, "blue");
+    resetAllBarColor(i + 1);    
+  }
+  resetAllBarColor();
+};
 
 //#endregion Sort functions
 
-main()
+main();
